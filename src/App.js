6402e002241ebf6c50c409afb057dc8fabd457d2
Indira Williams <indirajamurath@gmail.com>
2018-10-10 16:07:01 -0400

    able to get books and set state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
+import Book from './Book.js'
 
 class BooksApp extends React.Component {
   state = {
@@ -10,10 +11,44 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
+    current: [],
+    desired: [],
+    read: [],
     showSearchPage: false
   }
 
+  placeOnShelf = (books) => {
+    let currentBooks = []
+    let desiredBooks = []
+    let alreadyRead = []
+    books.forEach(book => {
+      if (book.shelf === 'currentlyReading'){
+        currentBooks.push(book)
+      }
+      else if (book.shelf === 'wantToRead'){
+        desiredBooks.push(book)
+      }
+      else if (book.shelf === 'read'){
+        alreadyRead.push(book)
+      }
+      else {
+        desiredBooks.push(book)
+      }
+    })
+
+    this.setState({
+      current: currentBooks, desired: desiredBooks, read: alreadyRead
+    })
+
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll()
+      .then(books => this.placeOnShelf(books))
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="app">
         {this.state.showSearchPage ? (
@@ -49,22 +84,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
-                        </div>
+                      <Book />
                       </li>
                       <li>
                         <div className="book">
